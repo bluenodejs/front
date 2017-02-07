2c024f642183fb46e78a5cbbc2dbf7ab19a3f8f6
Sergey Sova <i.am@lestad.net>
2017-02-07 16:49:51 +0300

    Draw lines

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,26 @@
 const createCanvas = () => ({
   html: d3.select('body').append('div')
     .classed('canvas', true)
-    .style('top', '5px')
-    .style('left', '5px'),
+    .style('top', '0px')
+    .style('left', '0px'),
   svg: d3.select('.canvas').append('svg')
-    .style('x', 5)
-    .style('y', 5)
+    .style('x', 0)
+    .style('y', 0)
 })
 
 const updateCanvasSize = ({ html, svg }) => (
   html
-    .style('height', `${document.documentElement.clientHeight - 10}px`)
-    .style('width', `${document.documentElement.clientWidth - 10}px`),
+    .style('height', `${document.documentElement.clientHeight}px`)
+    .style('width', `${document.documentElement.clientWidth}px`),
   svg
-    .style('height', `${document.documentElement.clientHeight - 10}px`)
-    .style('width', `${document.documentElement.clientWidth - 10}px`)
+    .style('height', `${document.documentElement.clientHeight}px`)
+    .style('width', `${document.documentElement.clientWidth}px`)
 )
 
 /**
  * @param panel {d3}
  * @param rowDef {Object}   { text, name, }
+ * @return {Object}  { row, text, point }
  */
 const createRow = (panel, rowDef, output = false) => {
   const row = panel.append('div')
@@ -29,13 +30,13 @@ const createRow = (panel, rowDef, output = false) => {
     ? row.attr('data-output', rowDef.name)
     : row.attr('data-input', rowDef.name)
 
-  row.append('span')
+  const text = row.append('span')
     .text(rowDef.text)
 
-  row.append('div')
+  const point = row.append('div')
     .classed('node-row-point', true)
 
-  return row
+  return { row, text, point, name: rowDef.name }
 }
 
 /**
@@ -58,10 +59,12 @@ const onNodeMove = node => () => {
 }
 
 /**
+ * @param canvas {Object}    { html, svg }
  * @param nodeDef {Object}   { name, }
+ * @return {Object}   { node, inputs, outputs, header }
  */
 const addNode = (canvas, nodeDef) => {
-  const node = canvas.append('div')
+  const node = canvas.html.append('div')
     .classed('node', true)
     .classed(`class-${nodeDef.type}`, !!nodeDef.type)
     .style('top', `${nodeDef.position.top || 20}px`)
@@ -75,8 +78,8 @@ const addNode = (canvas, nodeDef) => {
   const left = content.append('div').classed('node-panel panel-left', true)
   const right = content.append('div').classed('node-panel panel-right', true)
 
-  nodeDef.inputs && nodeDef.inputs.forEach(row => createRow(left, row))
-  nodeDef.outputs && nodeDef.outputs.forEach(row => createRow(right, row))
+  const inputs = nodeDef.inputs.map(row => createRow(left, row))
+  const outputs = nodeDef.outputs.map(row => createRow(right, row))
 
   header.call(
     d3.drag()
@@ -84,16 +87,58 @@ const addNode = (canvas, nodeDef) => {
       top: parseInt(node.style('top'), 10),
       left: parseInt(node.style('left'), 10),
     }))
-    .container(() => canvas.node())
+    .container(() => canvas.html.node())
     .filter(() => d3.event.target == header.node())
     .on('start', onNodeMove(node))
   )
 
-  return node
+  return { node, inputs, outputs, header, definition: nodeDef }
 }
 
-const createConnection = (canvas, nodeFrom, output, nodeTo, input) => {
+const getCenterOfPoint = point => {
+  const { top, left, width, height } = point.node().getBoundingClientRect()
+
+  return {
+    top: top + (height / 2),
+    left: left + (width / 2),
+  }
+}
+
+const makeLine = d3.line()
+  .x(d => d.left)
+  .y(d => d.top)
+  .curve(d3.curveCatmullRom.alpha(.8))
+
+const LINE_GROW_OFFSET = 20
+
+/**
+ * @param canvas {Object}   { html, svg }
+ * @param connection {Object}    { from, to, output, input }
+ * @return {Object}
+ */
+const createConnection = (canvas, { from, output, to, input }) => {
+  const inlet = to.inputs.filter(put => put.name === input)[0]
+  const outlet = from.outputs.filter(put => put.name === output)[0]
+  console.log({ from, output, to, input })
+  console.log({ inlet, outlet })
+
+  if (!inlet)
+    throw new Error(`Inlet "${input}" not found!`)
+  if (!outlet) 
+    throw new Error(`Outlet "${output}" not found!`)
+
+  const centerTo = getCenterOfPoint(inlet.point)
+  const centerFrom = getCenterOfPoint(outlet.point)
+
+  const connection = canvas.svg.append('path')
+    .attr('d', makeLine([
+      centerFrom,
+      { top: centerFrom.top, left: centerFrom.left + LINE_GROW_OFFSET },
+      { top: centerTo.top, left: centerTo.left - LINE_GROW_OFFSET },
+      centerTo,
+    ]))
   
+  return { connection, inlet, outlet }
 }
 
 // =======================================================================================
@@ -102,12 +147,13 @@ const canvas = createCanvas()
 updateCanvasSize(canvas)
 window.addEventListener('resize', () => updateCanvasSize(canvas))
 
-const node = addNode(canvas.html, {
+const node = addNode(canvas, {
   name: 'Horizontal interactor',
   position: {
     top: 50,
     left: 600,
   },
+  type: 'function',
   inputs: [{
     name: 'initial_value',
     text: 'Initial value',
@@ -121,7 +167,7 @@ const node = addNode(canvas.html, {
   }],
 })
 
-const node2 = addNode(canvas.html, {
+const node2 = addNode(canvas, {
   name: 'OnComponentBeginOverlap (Trigger Volume)',
   position: {
     top: 20,
@@ -141,7 +187,7 @@ const node2 = addNode(canvas.html, {
   }],
 })
 
-const node3 = addNode(canvas.html, {
+const node3 = addNode(canvas, {
   name: 'Make Vector',
   position: {
     top: 200,
@@ -164,9 +210,53 @@ const node3 = addNode(canvas.html, {
   }],
 })
 
+const nodeS = addNode(canvas, {
+  name: 'Split',
+  position: {
+    top: 190,
+    left: 50,
+  },
+  inputs: [{ name: 'input', text: 'Input' }],
+  outputs: [
+    { name: 'first', text: 'First Index' },
+    { name: 'second', text: 'Second Index' },
+  ]
+})
+
+
+const conn1 = createConnection(canvas, {
+  from: node2, output: 'other_body_index',
+  to: node3, input: 'x',
+})
+
+const conn2 = createConnection(canvas, {
+  from: node3, output: 'return_value',
+  to: node, input: 'contextual_resolver',
+})
+
+const conn3 = createConnection(canvas, {
+  from: node2, output: 'other_actor',
+  to: node, input: 'initial_value',
+})
+
+const conn4 = createConnection(canvas, {
+  from: node2, output: 'other_comp',
+  to: nodeS, input: 'input',
+})
+
+const conn5 = createConnection(canvas, {
+  from: nodeS, output: 'first',
+  to: node3, input: 'z',
+})
+
+const conn6 = createConnection(canvas, {
+  from: nodeS, output: 'second',
+  to: node3, input: 'y',
+})
+
 /*
 
-const node4 = addNode(canvas.html, {
+const node4 = addNode(canvas, {
   name: 'Add Actor Local Offset',
   position: {
     top: 150,
@@ -189,7 +279,7 @@ const node4 = addNode(canvas.html, {
   }],
 })
 
-const getter = addNode(canvas.html, {
+const getter = addNode(canvas, {
   name: 'Get (Inside Variable)',
   position: {
     top: 280,
@@ -203,7 +293,7 @@ const getter = addNode(canvas.html, {
   }],
 })
 
-const setter = addNode(canvas.html, {
+const setter = addNode(canvas, {
   name: 'Set (Inside Variable)',
   position: {
     top: 280,
